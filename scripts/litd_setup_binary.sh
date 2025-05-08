d3fe4179cc09a7151953826e0a11dbe805c55f4f
FoxtrotZulu <111104979+Foxtrot-Zulu@users.noreply.github.com>
2025-05-07 22:49:34 -0300

    New litd_setup_binary.sh
    spanish translation

--- scripts/litd_setup_binary.sh
+++ scripts/litd_setup_binary.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# litd Installation Script for Ubuntu
-# This script automates the installation and configuration of Lightning Terminal (litd)
+# Script de Instalación de litd para Ubuntu
+# Este script automatiza la instalación y configuración de Lightning Terminal (litd)
 
-set -e  # Exit immediately if a command exits with a non-zero status
+set -e  # Salir inmediatamente si un comando termina con un estado distinto de cero
 
 # Variables
 USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
@@ -13,7 +13,7 @@ LND_DIR="$USER_HOME/.lnd"
 WALLET_PASSWORD_FILE="$LND_DIR/wallet_password"
 SERVICE_FILE="/etc/systemd/system/litd.service"
 
-LITD_VERSION="v0.14.0-alpha"  # Version of litd to be installed
+LITD_VERSION="v0.14.0-alpha"  # Versión de litd a instalar
 BINARY_URL="https://github.com/lightninglabs/lightning-terminal/releases/download/$LITD_VERSION/lightning-terminal-linux-amd64-$LITD_VERSION.tar.gz"
 SIGNATURE_URL="https://github.com/lightninglabs/lightning-terminal/releases/download/$LITD_VERSION/manifest-guggero-$LITD_VERSION.sig"
 MANIFEST_URL="https://github.com/lightninglabs/lightning-terminal/releases/download/$LITD_VERSION/manifest-$LITD_VERSION.txt"
@@ -22,145 +22,145 @@ KEY_SERVER="hkps://keyserver.ubuntu.com"
 DOWNLOAD_DIR="/tmp/litd_release_verification"
 
 
-# Install litd from binary
-echo "[+] Checking if Lightning Terminal is already installed..."
+# Instalar litd desde el binario
+echo "[+] Verificando si Lightning Terminal ya está instalado..."
 if [[ -f "/usr/local/bin/litd" ]]; then
-    echo "[+] Lightning Terminal (litd) is already installed. Skipping installation."
+  echo "[+] Lightning Terminal (litd) ya está instalado. Omitiendo la instalación."
 else
-    echo "[+] litd not found in /usr/local/bin. Proceeding with installation."
+  echo "[+] litd no se encontró en /usr/local/bin. Procediendo con la instalación."
 
-    # Create download directory
-    mkdir -p "$DOWNLOAD_DIR"
-    cd "$DOWNLOAD_DIR" || { echo "Failed to navigate to download directory."; exit 1; }
-    echo "The current working directory is: $PWD"
+  # Crear directorio de descarga
+  mkdir -p "$DOWNLOAD_DIR"
+  cd "$DOWNLOAD_DIR" || { echo "No se pudo navegar al directorio de descarga."; exit 1; }
+  echo "El directorio de trabajo actual es: $PWD"
 
-    # Import Oli's key
-    echo "Importing Oli's key..."
-    gpg --keyserver "$KEY_SERVER" --recv-keys "$KEY_ID" || { echo "Failed to import PGP key."; exit 1; }
+  # Importar la clave de Oli
+  echo "Importando la clave de Oli..."
+  gpg --keyserver "$KEY_SERVER" --recv-keys "$KEY_ID" || { echo "No se pudo importar la clave PGP."; exit 1; }
 
-    # Download litd binary
-    echo "Downloading binary..."
-    wget "$BINARY_URL" || { echo "Failed to download binary."; exit 1; }
+  # Descargar el binario de litd
+  echo "Descargando el binario..."
+  wget "$BINARY_URL" || { echo "No se pudo descargar el binario."; exit 1; }
 
-    echo "Downloading signature..."
-    wget "$SIGNATURE_URL" || { echo "Failed to download signature."; exit 1; }
+  echo "Descargando la firma..."
+  wget "$SIGNATURE_URL" || { echo "No se pudo descargar la firma."; exit 1; }
 
-    echo "Downloading manifest..."
-    wget "$MANIFEST_URL" || { echo "Failed to download manifest."; exit 1; }
+  echo "Descargando el manifiesto..."
+  wget "$MANIFEST_URL" || { echo "No se pudo descargar el manifiesto."; exit 1; }
 
-    # Verify the release signature
-    echo "Verifying signature..."
-    gpg --verify "$(basename "$SIGNATURE_URL")" "$(basename "$MANIFEST_URL")" 2>&1 | grep "$KEY_ID" > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "Signature verification successful."
-    else
-        echo "Signature verification failed or does not match the expected key ID: $KEY_ID."
-        exit 1
-    fi
+  # Verificar la firma de la versión
+  echo "Verificando la firma..."
+  gpg --verify "$(basename "$SIGNATURE_URL")" "$(basename "$MANIFEST_URL")" 2>&1 | grep "$KEY_ID" > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Verificación de firma exitosa."
+  else
+    echo "La verificación de la firma falló o no coincide con el ID de clave esperado: $KEY_ID."
+    exit 1
+  fi
 
-    #Check SHASUM
-    echo "Checking shasum..."
-    grep "$(sha256sum "$(basename "$BINARY_URL")" | awk '{print $1}')" "$(basename "$MANIFEST_URL")" > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "SHA256 hash verification successful."
-    else
-        echo "SHA256 hash verification failed."
-        exit 1
-    fi
+  # Verificar SHASUM
+  echo "Verificando shasum..."
+  grep "$(sha256sum "$(basename "$BINARY_URL")" | awk '{print $1}')" "$(basename "$MANIFEST_URL")" > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Verificación de hash SHA256 exitosa."
+  else
+    echo "Verificación de hash SHA256 fallida."
+    exit 1
+  fi
 
-    echo "[+] Extracting litd binary..."
-    tar -xvzf "$DOWNLOAD_DIR/lightning-terminal-linux-amd64-$LITD_VERSION.tar.gz" -C "$DOWNLOAD_DIR" --strip-components=1
+  echo "[+] Extrayendo el binario litd..."
+  tar -xvzf "$DOWNLOAD_DIR/lightning-terminal-linux-amd64-$LITD_VERSION.tar.gz" -C "$DOWNLOAD_DIR" --strip-components=1
 
-    echo "[+] Moving binaries to /usr/local/bin..."
-    sudo mv "$DOWNLOAD_DIR"/* /usr/local/bin/
+  echo "[+] Moviendo los binarios a /usr/local/bin..."
+  sudo mv "$DOWNLOAD_DIR"/* /usr/local/bin/
 
-    echo "[+] Cleaning up temporary files..."
-    rm -rf "$DOWNLOAD_DIR"
+  echo "[+] Limpiando archivos temporales..."
+  rm -rf "$DOWNLOAD_DIR"
 
-    echo "[+] litd successfully installed!"
+  echo "[+] ¡litd instalado con éxito!"
 
-    # Change back to the ubuntu user's home directory
-    cd "$USER_HOME" || { echo "Failed to return to the user's home directory: $USER_HOME"; exit 1; }
+  # Volver al directorio de inicio del usuario de Ubuntu
+  cd "$USER_HOME" || { echo "No se pudo volver al directorio de inicio del usuario: $USER_HOME"; exit 1; }
 fi
 
-# Ensure ~/.lnd directory exists
-echo "[+] Ensuring the ~/.lnd directory exists..."
+# Asegurar que el directorio ~/.lnd existe
+echo "[+] Asegurando que el directorio ~/.lnd existe..."
 if [[ ! -d $LND_DIR ]]; then
-    mkdir -p $LND_DIR
-    echo "[+] Created directory at $LND_DIR."
-    # Ensure ~/.lnd directory is owned by the user
-    echo "[+] Ensuring ownership of $LND_DIR..."
-    sudo chown -R ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $LND_DIR
-    echo "[+] Ownership set to ${SUDO_USER:-$USER} for $LND_DIR."
+  mkdir -p $LND_DIR
+  echo "[+] Se creó el directorio en $LND_DIR."
+  # Asegurar que el directorio ~/.lnd es propiedad del usuario
+  echo "[+] Asegurando la propiedad de $LND_DIR..."
+  sudo chown -R ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $LND_DIR
+  echo "[+] Propiedad establecida en ${SUDO_USER:-$USER} para $LND_DIR."
 else
-    echo "[!] Directory $LND_DIR already exists."
+  echo "[!] El directorio $LND_DIR ya existe."
 fi
 
-# Generate wallet password
-echo "[+] Checking if wallet password file exists and is not empty..."
+# Generar contraseña de la billetera
+echo "[+] Verificando si el archivo de contraseña de la billetera existe y no está vacío..."
 if [[ -f $WALLET_PASSWORD_FILE && -s $WALLET_PASSWORD_FILE ]]; then
-    echo "[+] Wallet password file already exists and is not empty. Skipping generation."
+  echo "[+] El archivo de contraseña de la billetera ya existe y no está vacío. Omitiendo la generación."
 else
-    echo "[+] Generating wallet password..."
-    openssl rand -hex 21 > $WALLET_PASSWORD_FILE
-    if [[ -f $WALLET_PASSWORD_FILE ]]; then
-        echo "[+] Wallet password generated and saved to $WALLET_PASSWORD_FILE."
-        # Ensure wallet password file is owned by the user
-        echo "[+] Ensuring ownership of $WALLET_PASSWORD_FILE..."
-        sudo chown ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $WALLET_PASSWORD_FILE
-        echo "[+] Ownership set to ${SUDO_USER:-$USER} for $WALLET_PASSWORD_FILE."
-    else
-        echo "[-] Failed to generate wallet password. Exiting."
-        exit 1
-    fi
+  echo "[+] Generando contraseña de la billetera..."
+  openssl rand -hex 21 > $WALLET_PASSWORD_FILE
+  if [[ -f $WALLET_PASSWORD_FILE ]]; then
+    echo "[+] Contraseña de la billetera generada y guardada en $WALLET_PASSWORD_FILE."
+    # Asegurar que el archivo de contraseña de la billetera es propiedad del usuario
+    echo "[+] Asegurando la propiedad de $WALLET_PASSWORD_FILE..."
+    sudo chown ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $WALLET_PASSWORD_FILE
+    echo "[+] Propiedad establecida en ${SUDO_USER:-$USER} para $WALLET_PASSWORD_FILE."
+  else
+    echo "[-] No se pudo generar la contraseña de la billetera. Saliendo."
+    exit 1
+  fi
 fi
 
-# Configure litd
-echo "[+] Step 4: Configuring Lightning Terminal (litd)..."
+# Configurar litd
+echo "[+] Paso 4: Configurando Lightning Terminal (litd)..."
 
-# Check if configuration directory exists
+# Verificar si el directorio de configuración existe
 if [[ ! -d $LIT_CONF_DIR ]]; then
-    mkdir -p $LIT_CONF_DIR
-    echo "[+] Created configuration directory at $LIT_CONF_DIR."
-    # Ensure .lit directory is owned by the user
-    echo "[+] Ensuring ownership of $LIT_CONF_DIR..."
-    sudo chown -R ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $LIT_CONF_DIR
-    echo "[+] Ownership set to ${SUDO_USER:-$USER} for $LIT_CONF_DIR."
+  mkdir -p $LIT_CONF_DIR
+  echo "[+] Se creó el directorio de configuración en $LIT_CONF_DIR."
+  # Asegurar que el directorio .lit es propiedad del usuario
+  echo "[+] Asegurando la propiedad de $LIT_CONF_DIR..."
+  sudo chown -R ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $LIT_CONF_DIR
+  echo "[+] Propiedad establecida en ${SUDO_USER:-$USER} para $LIT_CONF_DIR."
 else
-    echo "[!] $LIT_CONF_DIR already exists."
+  echo "[!] $LIT_CONF_DIR ya existe."
 fi
 
-# Check if configuration file exists and is not empty
+# Verificar si el archivo de configuración existe y no está vacío
 if [[ -f $LIT_CONF_FILE && -s $LIT_CONF_FILE ]]; then
-    echo "[+] Configuration file already exists and is not empty. Skipping creation."
+  echo "[+] El archivo de configuración ya existe y no está vacío. Omitiendo la creación."
 else
-    echo "[+] Generating new configuration file..."
+  echo "[+] Generando nuevo archivo de configuración..."
 
-    read -p "Is your bitcoind backend running on mainnet or signet? [mainnet/signet]: " NETWORK
-    NETWORK=$(echo "$NETWORK" | tr '[:upper:]' '[:lower:]')
-    if [[ $NETWORK != "mainnet" && $NETWORK != "signet" ]]; then
-        echo "[-] Invalid network selection. Please choose either 'mainnet' or 'signet'."
-        exit 1
-    fi
+  read -p "¿Su backend de bitcoind se está ejecutando en mainnet o signet? [mainnet/signet]: " NETWORK
+  NETWORK=$(echo "$NETWORK" | tr '[:upper:]' '[:lower:]')
+  if [[ $NETWORK != "mainnet" && $NETWORK != "signet" ]]; then
+    echo "[-] Selección de red inválida. Por favor, elija 'mainnet' o 'signet'."
+    exit 1
+  fi
 
-    read -s -p "Enter the RPC password for your bitcoind backend: " RPC_PASSWORD
-    echo
-    if [[ -z $RPC_PASSWORD ]]; then
-        echo "[-] RPC password cannot be empty. Exiting."
-        exit 1
-    fi
+  read -s -p "Ingrese la contraseña RPC para su backend de bitcoind: " RPC_PASSWORD
+  echo
+  if [[ -z $RPC_PASSWORD ]]; then
+    echo "[-] La contraseña RPC no puede estar vacía. Saliendo."
+    exit 1
+  fi
 
-    read -s -p "Enter a UI password for litd: " UI_PASSWORD
-    echo
-    if [[ -z $UI_PASSWORD ]]; then
-        echo "[-] UI password cannot be empty. Exiting."
-        exit 1
-    fi
+  read -s -p "Ingrese una contraseña de UI para litd: " UI_PASSWORD
+  echo
+  if [[ -z $UI_PASSWORD ]]; then
+    echo "[-] La contraseña de la UI no puede estar vacía. Saliendo."
+    exit 1
+  fi
 
-    read -p "Enter a Lightning Node alias: " NODE_ALIAS
+  read -p "Ingrese un alias de nodo Lightning: " NODE_ALIAS
 
-    # Prepare the base configuration content
-    CONFIG_CONTENT="# Litd Settings
+  # Preparar el contenido de configuración base
+  CONFIG_CONTENT="# Configuración de Litd
 enablerest=true
 httpslisten=0.0.0.0:8443
 uipassword=$UI_PASSWORD
@@ -170,7 +170,7 @@ pool-mode=disable
 loop-mode=disable
 autopilot.disable=true
 
-# Bitcoin Configuration
+# Configuración de Bitcoin
 lnd.bitcoin.active=1
 lnd.bitcoin.node=bitcoind
 lnd.bitcoind.rpchost=127.0.0.1
@@ -179,7 +179,7 @@ lnd.bitcoind.rpcpass=$RPC_PASSWORD
 lnd.bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332
 lnd.bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333
 
-# LND General Settings
+# Configuración General de LND
 #lnd.wallet-unlock-password-file=/home/ubuntu/.lnd/wallet_password
 #lnd.wallet-unlock-allow-create=true
 lnd.debuglevel=debug
@@ -190,14 +190,14 @@ lnd.accept-amp=true
 lnd.rpcmiddleware.enable=true
 lnd.autopilot.active=0
 
-# LND Protocol Settings
+# Configuración del Protocolo LND
 lnd.protocol.simple-taproot-chans=true
 lnd.protocol.simple-taproot-overlay-chans=true
 lnd.protocol.option-scid-alias=true
 lnd.protocol.zero-conf=true
 lnd.protocol.custom-message=17
 
-# Taproot Assets Settings
+# Configuración de Activos Taproot
 #taproot-assets.rpclisten=0.0.0.0:10029
 #taproot-assets.allow-public-uni-proof-courier=true
 #taproot-assets.allow-public-stats=true
@@ -207,24 +207,24 @@ lnd.protocol.custom-message=17
 #taproot-assets.experimental.rfq.priceoracleaddress=use_mock_price_oracle_service_promise_to_not_use_on_mainnet
 #taproot-assets.experimental.rfq.mockoracleassetsperbtc=100000000"
 
-    # Apply mainnet-specific logic
-    if [[ $NETWORK == "mainnet" ]]; then
-        CONFIG_CONTENT=$(echo "$CONFIG_CONTENT" | sed "/pool-mode=disable/s/^/# /" | sed "/loop-mode=disable/s/^/# /" | sed "/autopilot.disable=true/s/^/# /")
-    fi
+  # Aplicar lógica específica de mainnet
+  if [[ $NETWORK == "mainnet" ]]; then
+    CONFIG_CONTENT=$(echo "$CONFIG_CONTENT" | sed "/pool-mode=disable/s/^/# /" | sed "/loop-mode=disable/s/^/# /" | sed "/autopilot.disable=true/s/^/# /")
+  fi
 
-    # Write configuration content to file
-    echo "$CONFIG_CONTENT" > $LIT_CONF_FILE
-    echo "[+] Configuration file created at $LIT_CONF_FILE."
+  # Escribir contenido de configuración en el archivo
+  echo "$CONFIG_CONTENT" > $LIT_CONF_FILE
+  echo "[+] Archivo de configuración creado en $LIT_CONF_FILE."
 
-    # Ensure configuration file is owned by the user
-    echo "[+] Ensuring ownership of $LIT_CONF_FILE..."
-    sudo chown ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $LIT_CONF_FILE
-    echo "[+] Ownership set to ${SUDO_USER:-$USER} for $LIT_CONF_FILE."
+  # Asegurar que el archivo de configuración es propiedad del usuario
+  echo "[+] Asegurando la propiedad de $LIT_CONF_FILE..."
+  sudo chown ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} $LIT_CONF_FILE
+  echo "[+] Propiedad establecida en ${SUDO_USER:-$USER} para $LIT_CONF_FILE."
 fi
 
-echo "Now you have a task! Start litd with $ litd, do so as the user who will be running litd."
-echo "In a new tab..."
-echo "Walk through the wallet creation process using $ lncli --network=[yournetwork] create."
-echo "Use the already generated password which can be found via $ cat ~/.lnd/wallet_password"
-echo "DO NOT FORGET TO PROPERLY BACKUP YOUR SEED!!!" 
-echo "Then, stop litd, and run the next script... almost there!!!"
+echo "Ahora tienes una tarea! Inicia litd con $ litd, hazlo como el usuario que ejecutará litd."
+echo "En una nueva pestaña..."
+echo "Sigue el proceso de creación de la billetera usando $ lncli --network=[tured] create."
+echo "Usa la contraseña ya generada que se puede encontrar a través de $ cat ~/.lnd/wallet_password"
+echo "¡¡¡NO OLVIDES RESPALDAR ADECUADAMENTE TU SEMILLA!!!"
+echo "Luego, detén litd y ejecuta el siguiente script... ¡ya casi terminamos!!!"
